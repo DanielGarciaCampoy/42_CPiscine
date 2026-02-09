b1bb08144af72830078fac06037f44b5a3974977
DanielGarciaCampoy <daniel.garcia.campoy@gmail.com>
2026-02-09 19:42:22 +0000

    ex04 hecho

---
+++ C02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int	is_lowercase(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+int	ft_str_is_lowercase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (is_lowercase(str[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_str_is_lowercase(""));
+	printf("%d\n", ft_str_is_lowercase("prueba"));
+	printf("%d\n", ft_str_is_lowercase("prueba1"));
+	printf("%d\n", ft_str_is_lowercase("prueba1-."));
+	return (0);
+}
