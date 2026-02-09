3a09a2ea8b4b7633cac0502b0a19e2fe0021b5ec
DanielGarciaCampoy <daniel.garcia.campoy@gmail.com>
2026-02-09 20:23:00 +0000

    C02: ex06 hecho

---
+++ C02/ex06/ft_str_is_printable.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int	is_printable(char c)
+{
+	return (c >= 32 && c <= 126);
+}
+
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (is_printable (str[i]))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_str_is_printable("aa"));
+	printf("%d\n", ft_str_is_printable("prueba*"));
+	printf("%d\n", ft_str_is_printable("aña"));
+	return (0);
+}
